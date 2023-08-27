c4b3a132041c5218571349afbe018cc06fd6e3cd
guicoelhodev <gs.coelho@outlook.com>
2023-08-27 14:52:44 -0300

    feat: create basic apollo server client

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,41 @@
-console.log("Hello world aa");
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+const typeDefs = `#graphql
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 8080 },
+});
+
+console.log("Server running on:", url);
