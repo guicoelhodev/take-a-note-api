3eb8dfcb9139976515830e03b8c7283c87a85a66
guicoelhodev <gs.coelho@outlook.com>
2023-08-27 16:36:31 -0300

    refactor: add graphq types and change apollo server

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,29 @@
+import "reflect-metadata";
+
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-let usersData = [];
+import { UsersResolver } from "./resolvers/Users.ts";
+import { buildSchema } from "type-graphql";
+import path, { dirname } from "node:path";
 
-const typeDefs = `#graphql
+import { fileURLToPath } from "url";
 
-  type Query {
-    users: [String!]
-  }
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-  type Mutation {
-    createUser(name: String!): String!
-  }
-`;
+async function main() {
+  const schema = await buildSchema({
+    resolvers: [UsersResolver],
+    emitSchemaFile: path.resolve(__dirname, "schema.gql"),
+  });
+  const server = new ApolloServer({ schema });
 
-const resolvers = {
-  Query: {
-    users: usersData,
-  },
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 8080 },
+  });
 
-  Mutation: {
-    createUser: (parent, args) => {
-      usersData.push(args.name);
-      return "User created succesfully!";
-    },
-  },
-};
+  console.log("Server running on:", url);
+}
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 8080 },
-});
-
-console.log("Server running on:", url);
+main();
