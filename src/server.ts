a9badf088c5f4d9886c01e60596470629434840b
guicoelhodev <gs.coelho@outlook.com>
2023-08-27 15:27:19 -0300

    feat: create base example graphQL

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,29 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
+let usersData = [];
 
 const typeDefs = `#graphql
-  type Book {
-    title: String
-    author: String
-  }
 
   type Query {
-    books: [Book]
+    users: [String!]
+  }
+
+  type Mutation {
+    createUser(name: String!): String!
   }
 `;
 
 const resolvers = {
   Query: {
-    books: () => books,
+    users: usersData,
+  },
+
+  Mutation: {
+    createUser: (parent, args) => {
+      usersData.push(args.name);
+      return "User created succesfully!";
+    },
   },
 };
 
