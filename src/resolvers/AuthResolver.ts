412f7d85e13b4d2e0a806a0b55f73ef8a1752a34
guicoelhodev <gs.coelho@outlook.com>
2023-08-28 22:37:38 -0300

    feat: add new mutation on AuthResolver, logIn and sendJwtToken mutations

--- src/resolvers/AuthResolver.ts
+++ src/resolvers/AuthResolver.ts
@@ -1,20 +1,34 @@
 import { Arg, Mutation, Resolver } from "type-graphql";
-import { AuthProviderInput } from "../dtos/inputs/AuthProviderInput.ts";
+import {
+  AuthJwtInput,
+  AuthProviderInput,
+} from "../dtos/inputs/AuthProviderInput.ts";
 import { supabase } from "../server.ts";
+import { AuthGithubModel } from "../dtos/models/AuthGithubModel.ts";
 
 @Resolver()
 export class AuthResolver {
-  @Mutation(() => Boolean!)
-  async logIn(@Arg('body') { provider }: AuthProviderInput) {
-    const { data, error } = await supabase.auth.signInWithOAuth({
+  @Mutation(() => String!)
+  async sendJwtToken(@Arg("body") { provider }: AuthProviderInput) {
+    const response = await supabase.auth.signInWithOAuth({
       provider: provider,
-    })
+      options: {
+        scopes: "read:user",
+        redirectTo: "http://localhost:3000/login",
+      },
+    });
 
-    if(error) {
-      throw new Error(error.message)
+    if (response.error) {
+      throw new Error(response.error.message);
     }
 
-    console.log(data)
-    return true;
+    return response.data.url;
+  }
+
+  @Mutation(() => AuthGithubModel!)
+  async logIn(@Arg("body") { jwtToken }: AuthJwtInput) {
+    const { data, error } = await supabase.auth.getUser(jwtToken);
+
+    return data.user;
   }
 }
